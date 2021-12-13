3e837837b6fb08d8db0f0923f523b0582a2325ab
FernandoGaspari <bliitzx@gmail.com>
2021-12-12 23:22:48 -0300

    fernando

--- src/main/resources/static/js/vue-productos.js
+++ src/main/resources/static/js/vue-productos.js
@@ -1,13 +1,16 @@
 const app = new Vue({
-    el: "#app-producto",
+    el: "#app",
     data: {
         productos: [],
+        categorias: [],
         errored: false,
         loading: true
     },
     created() {
         var url = "http://localhost:8080/api/get/productos"
+
         this.fetchData(url);
+        this.fetchData1(url1);
     },
     methods: {
         fetchData(url) {
@@ -18,11 +21,28 @@ const app = new Vue({
                     console.log(data);
                     this.loading = false;
                     console.log(this.loading);
+                    var url1 = "http://localhost:8080/api/get/categorias"
+                    fetch(url1)
+                        .then(response => response.json())
+                        .then(data => {
+                            this.categorias = data;
+                            console.log(data);
+                            this.loading = false;
+                            console.log(this.loading);
+                        })
+                        .catch(err => {
+                            this.errored = true
+                            console.error(err);
+                        })
                 })
                 .catch(err => {
                     this.errored = true
                     console.error(err);
                 })
+        },
+
+        fetchData1(url1) {
+
         }
     }
 })
