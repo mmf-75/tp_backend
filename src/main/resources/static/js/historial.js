d442b16053a8234ba22eb04b44c192d2e4dc7349
MatiasEsp404 <unity.matias@outlook.com>
2021-12-19 21:56:29 -0300

    Historial de compras del usuario

--- src/main/resources/static/js/historial.js
+++ src/main/resources/static/js/historial.js
@@ -2,27 +2,12 @@ var datos = new Vue({
     data:{
         categorias: [],
         cliente: {},
-        // total: 0,
-        // producto: {}
-        ventas: [],
     },
     created() {
         this.cargaCategorias("http://localhost:8080/api/get/categorias/")
         this.cargaCliente("http://localhost:8080/api/get/clientes/2")
-        this.cargaVentas("http://localhost:8080/api/get/ventas/2")
-        // this.cargaProducto("http://localhost:8080/api/get/productos/1")
     },
     methods: {
-        cargaVentas(url){
-            fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                this.ventas = data
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        },
         cargaCategorias(url) {
             fetch(url)
                 .then(res => res.json())
@@ -38,38 +23,12 @@ var datos = new Vue({
                 .then(res => res.json())
                 .then(data => {
                     this.cliente = data
-                    console.log(data);
+                    console.log(this.cliente.ventas);
                 })
-                // .then(()=>{
-                //     for (let i = 0; i < this.cliente.carrito.length; i++) {
-                //         this.total += this.cliente.carrito[i].precio
-                //     }
-                // })
                 .catch(err => {
                     console.log(err)
                 })
         },
-        // cargaProducto(url) {
-        //     fetch(url)
-        //         .then(res => res.json())
-        //         .then(data => {
-        //             this.productos = data
-        //             console.table(this.productos)
-        //         })
-        //         .catch(err => {
-        //             console.log(err)
-        //         })
-        // },
-        // sacarDelCarrito(idProducto){
-        //     const url = `http://localhost:8080/api/${this.cliente.id}/carrito/${idProducto}`
-        //     const opciones = {
-        //         method: 'DELETE'
-        //     }
-        //     fetch(url, opciones)
-        //         .then(res => res.text())
-        //         .then(() => location.reload())
-        //         .catch(err => console.error(err))
-        // }
     }
 })
 
