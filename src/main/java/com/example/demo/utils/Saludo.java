914f1ccb4d41bda2af60dafeb04f6de85b3c8c02
MatiasEsp404 <unity.matias@outlook.com>
2021-12-24 04:04:02 -0300

    Create Saludo.java

---
+++ src/main/java/com/example/demo/utils/Saludo.java
@@ -0,0 +1,14 @@
+package com.example.demo.utils;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Saludo {
+
+    @GetMapping(path = "/")
+    public String saludo(){
+        return "hola";
+    }
+
+}
