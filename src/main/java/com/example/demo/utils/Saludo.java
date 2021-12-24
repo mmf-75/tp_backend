83e1c0c2c4ce71dc92ca3403eac05ee82dabd8b4
MatiasEsp404 <unity.matias@outlook.com>
2021-12-24 10:22:08 -0300

    Pruebas

--- src/main/java/com/example/demo/utils/Saludo.java
+++ src/main/java/com/example/demo/utils/Saludo.java
@@ -1,12 +1,12 @@
 package com.example.demo.utils;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+// @RestController
 public class Saludo {
 
-    @GetMapping(path = "/")
+    // @GetMapping(path = "/")
     public String saludo(){
         return "hola";
     }
